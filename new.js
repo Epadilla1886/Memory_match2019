5d7e325ada5c7120c5ee169d57d85b7ebe2d5769
Epadilla1886 <ejpadilla1886@gmail.com>
2019-01-07 13:51:44 -0800

    Memory Match version1.0 - EJ Padilla

--- new.js
+++ new.js
@@ -1,18 +1,23 @@
-
-
 var cardList = null;
-var flippedCard = false
+var flippedCard = false;
 var cardOne = null;
-var cardTwo= null;
+var cardTwo = null;
+var cardMatch = 0;
 var boardDisabled = false;
 var lifepoints = 100;
 var life = null;
-
-
+var gamesWon = 0;
+var gamesLost = 0;
+var gamesPlayed = 0;
+var attempts = 0;
+var accuracy = 100;
+var possibleMatches = 9;
 
 window.onload = function(){
+    display_stats();
     loadCards();
-}
+    }
+
 var loadCards = function loadingCards(){
     cardList = document.querySelectorAll('.card');
     cardList.forEach(iter => iter.addEventListener('click', flipCard));
@@ -28,11 +33,13 @@ function flipCard(){
     if(!flippedCard){
         flippedCard = true;
         cardOne = this;
+        cardOne.removeEventListener('click', flipCard);
         return;
     }
     cardTwo = this;
     flippedCard = false;
-    checkForMatch();
+    cardOne.addEventListener('click', flipCard);
+    setTimeout(checkForMatch, 500);
 }
 
 function checkForMatch(){
@@ -40,42 +47,106 @@ function checkForMatch(){
     if(cardOne.dataset.card===cardTwo.dataset.card){
         lifepoints += 5;
         life.value = lifepoints;
+        attempts++;
+        cardMatch++;
+        checkForEnd();
         disableCards();
+        calculate();
+        display_stats();
         return;
     } else
         { lifepoints -= 10;
-            life.value = lifepoints;}
+            life.value = lifepoints;
+            attempts++;
+            checkForEnd();
+            calculate();
+            display_stats();
+        }
     unflip();
 }
 
-function unflip(){
-    boardDisabled = true;
-    setTimeout(()=> {
-        cardOne.classList.remove('flip');
-        cardTwo.classList.remove('flip');
-        boardDisabled = false;
-    } ,1500);
-}
-
-function disableCards(){
-    cardOne.removeEventListener('click', flipCard);
-    cardTwo.removeEventListener('click', flipCard);
-}
-
-function randomShuff(){
-    cardList.forEach(card =>{
-        var randomNum = Math.floor(Math.random() * cardList.length)
-        card.style.order = randomNum;
-            });
-}
-
-function victory(){
-
-}
-
-
-function gameOver(){
-    if(life.value = 0){
-        alert("K.O.!");
+    function checkForEnd() {
+        if (lifepoints > 0 && cardMatch === 9) {
+            return victory();
+    }  else if (lifepoints === 0) {
+            return gameOver();
     }
+}
+
+    function unflip() {
+        boardDisabled = true;
+            setTimeout(() => {
+                cardOne.classList.remove('flip');
+                cardTwo.classList.remove('flip');
+                boardDisabled = false;
+        }, 1000);
+    }
+
+    function disableCards() {
+        cardOne.removeEventListener('click', flipCard);
+        cardTwo.removeEventListener('click', flipCard);
+    }
+
+    function randomShuff() {
+        cardList.forEach(card => {
+            var randomNum = Math.floor(Math.random() * cardList.length);
+            card.style.order = randomNum;
+        });
+    }
+
+    function victory() {
+    if (lifepoints > 0 && cardMatch === 9);
+        stopAudio();
+        gamesWon++;
+        gameTotal();
+        display_stats();
+        alert("Victory !");
+    }
+
+    function gameOver() {
+    if (lifepoints === 0);
+        gamesLost++;
+        gameTotal();
+        display_stats();
+        alert('K.O. !');
+    }
+
+    function clearBoard() {
+        $('.flip').removeClass('flip');
+    }
+
+    function resetGame() {
+        gamesPlayed++;
+        clearBoard();
+        setTimeout(loadCards, 300);
+        resetStats();
+        display_stats();
+        $("#life").val(100);
+
+}
+function resetStats() {
+    attempts = 0;
+    accuracy = 100;
+    lifepoints = 100;
+    cardMatch = 0;
+}
+
+    function display_stats() {
+        $('.games-played .value').text(gamesPlayed);
+        $('.attempts .value').text(attempts);
+        $('.accuracy .value').text(accuracy + '%');
+    }
+
+    function calculate() {
+        var possibleMatches = 9;
+        accuracy = Math.floor((possibleMatches / attempts) * 10);
+    }
+
+    function gameTotal() {
+        gamesPlayed = gamesWon + gamesLost;
+        return gamesPlayed;
+    }
+
+function toggleMute(){
+    $("#audio_player").prop("muted",!$("#audio_player").prop("muted"));
 }
